6427b2bbccf66140c72dc2a0c2cefbef91eb52c7
James Birchall <jamesbirchall@gmail.com>
2016-10-08 19:03:04 +0100

    Chapter 1 - started
    added storyboard elements to view and started code side

---
+++ Quiz/Quiz/QuizViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by James Birchall on 08/10/2016.
+//  Copyright © 2016 James Birchall. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+}
+
