1a7ae71eccda9a409d140b9587eb77550fdda549
James Birchall <jamesbirchall@gmail.com>
2016-10-12 19:48:55 +0100

    Chapter 4 completed
    Chapter 4 completed including using CharacterSet to limit type of data
    entry into the field based on delegate method

---
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by James Birchall on 12/10/2016.
+//  Copyright © 2016 James Birchall. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelciusLabel()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        } else {
+            return nil
+        }
+    }
+    
+    var numberFormatter: NumberFormatter {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 2
+        return nf
+    }
+
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    @IBAction func farhFieldEditingChanged(textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = value
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard() {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelciusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(for: value)
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        
+        // look for character in string - if found return false straight away!
+        // bronze challenge
+        let isDigit = string.rangeOfCharacter(from: CharacterSet.decimalDigits)
+
+        let existingTextHasDecimalSeperator = textField.text?.range(of: ".")
+        
+        let replacementTextHasDecimalSeperator = string.range(of: ".")
+        
+        if isDigit == nil {
+            
+            // check character isn't "."
+            if string != "." {
+                return false
+            }
+        }
+        
+        if existingTextHasDecimalSeperator != nil && replacementTextHasDecimalSeperator != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+}
