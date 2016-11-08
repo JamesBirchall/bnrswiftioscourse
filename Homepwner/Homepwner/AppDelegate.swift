57504c70b55ea34700969f5635c303bd2781f6e2
James Birchall <jamesbirchall@gmail.com>
2016-11-08 17:18:34 +0000

    application bundle resources
    how to see packaged bundle and programmatically accessing them at
    runtime - query for the resource

--- Homepwner/Homepwner/AppDelegate.swift
+++ Homepwner/Homepwner/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        print(Bundle.main.bundlePath)
+        
         print(#function)
         
         // init and share itemStore object with itemsViewController
